e6a006095fc381c00040c51208c98809979c737b
highsource <aleksei.valikov@gmail.com>
2016-04-06 22:28:54 +0200

    Removed change probability.

--- core/src/main/java/org/hisrc/azw/integration/impl/v_1_0/DefaultApiFacilityToFacilityStateReportConverter.java
+++ core/src/main/java/org/hisrc/azw/integration/impl/v_1_0/DefaultApiFacilityToFacilityStateReportConverter.java
@@ -13,22 +13,23 @@ public class DefaultApiFacilityToFacilityStateReportConverter {
 
 	private GeometryFactory geometryFactory = new GeometryFactory();
 
-	private double changeProbability = 0;
-
-	public double getChangeProbability() {
-		return changeProbability;
-	}
-
-	public void setChangeProbability(double changeProbability) {
-		this.changeProbability = changeProbability;
-	}
-
+//	private double changeProbability = Double.MIN_VALUE;
+//
+//	public double getChangeProbability() {
+//		return changeProbability;
+//	}
+//
+//	public void setChangeProbability(double changeProbability) {
+//		this.changeProbability = changeProbability;
+//	}
+//
 	public FacilityStateReport asFacilityStateReport(org.hisrc.dbeac.client.v_1_0.model.Facility f) {
 
 		final Facility facility = asFacility(f);
 		final FacilityState facilityState;
-		final double changeProbability = getChangeProbability();
-		final boolean shouldNotBeFaked = Math.random() >= changeProbability;
+//		final double changeProbability = getChangeProbability();
+//		final boolean shouldNotBeFaked = Math.random() >= changeProbability;
+		final boolean shouldNotBeFaked = true;
 		switch (f.getState()) {
 		case ACTIVE:
 			facilityState = shouldNotBeFaked ? FacilityState.ACTIVE : FacilityState.TEST_ACTIVE;
